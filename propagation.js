2a239e544d1a9125c1d28564e01c2a8377db5108
BertrandBorel <95342688+BertrandBorel@users.noreply.github.com>
2022-11-04 10:12:06 +0100

    Add files via upload

--- propagation.js
+++ propagation.js
@@ -1,4 +1,6 @@
-// Création d'un réseau de neurones avec Javascript
+// Création d'un réseau de neurones avec Javascript :
+// Propagation des données à l'intérieur du réseau de neurones, de l'input à l'output.
+
 // Source : 
 // HEUDIN J.-C., Comprendre le Deep Learning, une introduction aux réseaux de neurones, Science-eBook, Paris, 2016, ISBN:979-1091245449, p.87-117.
 
